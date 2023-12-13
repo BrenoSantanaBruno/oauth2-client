4615210d4c521eb0a2d7eff4d12cdab1f72c5f1e
Breno Santana Bruno <brenosantanabruno@live.com>
2023-12-13 15:35:42 -0300

    Funcionando Corretamente

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('prepare-to-login', function () {
+   $state = Str::random(40);
+    $query = http_build_query([
+         'client_id' => env('CLIENT_ID'),
+         'redirect_uri' => env('REDIRECT_URL'),
+         'response_type' => 'code',
+         'scope' => '',
+         'state' => $state,
+    ]);
+    return redirect('http://localhost:8000/oauth/authorize?'.$query);
+})->name('prepare-to-login');
+
+Route::get('callback', function (Request $request) {
+//    $http = new GuzzleHttp\Client;
+//    $response = $http->post('http://localhost:8000/oauth/token', [
+//         'form_params' => [
+//             'grant_type' => 'authorization_code',
+//             'client_id' => 3,')
+    dd($request->all());
 });
